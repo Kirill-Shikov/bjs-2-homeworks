88449ce17ff2cccb08f5ce2a0879cfd0ea4225c8
Kirill Shikov <rbhz2806@mail.ru>
2025-11-10 20:31:43 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,95 @@
 function getArrayParams(...arr) {
 
-  return { min: min, max: max, avg: avg };
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		const current = arr[i];
+		if (current < min) {
+			min = current;
+		}
+		if (current > max) {
+			max = current;
+		}
+
+		sum += current;
+	}
+
+	const avg = Number((sum / arr.length).toFixed(2));
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
 
 function summElementsWorker(...arr) {
-
+	if (arr.length === 0) return 0;
+	return arr.reduce((sum, current) => sum + current, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
 
+	const max = Math.max(...arr);
+	const min = Math.min(...arr);
+	return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
 
+	for (let i = 0; i < arr.length; i++) {
+		const current = arr[i];
+
+		if (current % 2 === 0) {
+			sumEvenElement += current;
+		} else {
+			sumOddElement += current;
+		}
+	}
+	return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+	if (arr.length === 0) {
+		return 0;
+	}
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
 
+	for (let i = 0; i < arr.length; i++) {
+		const current = arr[i];
+
+		if (current % 2 === 0) {
+			sumEvenElement += current;
+			countEvenElement++;
+		}
+	}
+	if (countEvenElement === 0) {
+		return 0;
+	}
+	const average = Number((sumEvenElement / countEvenElement).toFixed(2));
+	return average;
 }
 
-function makeWork (arrOfArr, func) {
+function makeWork(arrOfArr, func) {
+	let maxResult = -Infinity;
 
+	for (let i = 0; i < arrOfArr.length; i++) {
+		const currentResult = func(...arrOfArr[i]);
+
+		if (currentResult > maxResult) {
+			maxResult = currentResult;
+		}
+	}
+
+	return maxResult;
 }
